5a685e53926f08a521d6962a77b98d75f3be6df6
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-05-02 17:54:18 +0100

    feat: domain registry drafted

--- contracts/PermitRegistry.sol
+++ contracts/PermitRegistry.sol
@@ -8,16 +8,21 @@ import "dss-interfaces/src/dss/DaiAbstract.sol";
 
 
 interface IERC1271 {
-  /**
-   * @dev Should return whether the signature provided is valid for the provided data
-   * @param hash      Hash of the data to be signed
-   * @param signature Signature byte array associated with _data
-   */
+  /// @dev Should return whether the signature provided is valid for the provided data
+  /// @param hash      Hash of the data to be signed
+  /// @param signature Signature byte array associated with _data
   function isValidSignature(bytes32 hash, bytes memory signature) external view returns (bytes4 magicValue);
 }
 
+interface IERC712 {
+    /// @dev Return the ERC712 domain separator
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+}
+
+
 contract PermitRegistry {
     event PermitRegistered(address indexed token, uint8 indexed permitType);
+    event DomainRegistered(address indexed token, string indexed name, string indexed version, bytes32 domain);
 
     uint8 constant public NONE = 0;
     uint8 constant public DAI = 1;
@@ -27,6 +32,36 @@ contract PermitRegistry {
     uint256 public constant MAX = type(uint256).max;
 
     mapping (address => uint8) public permits;
+    mapping (address => string) public names;
+    mapping (address => string) public versions;
+    mapping (address => bytes32) public domains;
+
+    /// @dev Return all known data from a given address.
+    function data(address token) public view returns (uint8, string memory, string memory, bytes32) {
+        return (permits[token], names[token], versions[token], domains[token]);
+    }
+
+    /// @dev Verify if the parameters were used to build a domain separator in a target contract, and store everything if positive.
+    function registerDomain(address token, string memory name, string memory version) public {
+        uint256 chainId;
+        assembly {chainId := chainid()}
+
+        bytes32 domain = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(name)),
+                keccak256(bytes(version)),
+                chainId,
+                token
+            )
+        );
+
+        require (domain == IERC712(token).DOMAIN_SEPARATOR(), "Mismatched domains");
+        names[token] = name;
+        versions[token] = version;
+        domains[token] = domain;
+        emit DomainRegistered(token, name, version, domain);
+    }
 
     /// @dev Use an ERC2612 permit to register if `token` implements it.
     /// The permit should have this contract as the spender, and type(uint256).max as allowance and deadline.
@@ -55,7 +90,7 @@ contract PermitRegistry {
         emit PermitRegistered(token, ERC1271);
     }
 
-    /// @dev se an Dai-style permit to register if `token` implements it.
+    /// @dev Use an Dai-style permit to register if `token` implements it.
     /// The permit should have this contract as the spender, and type(uint256).max as deadline.
     /// @notice As a precaution, set the allowance back to zero after registering.
     function registerDaiPermit(address token, uint8 v, bytes32 r, bytes32 s)
